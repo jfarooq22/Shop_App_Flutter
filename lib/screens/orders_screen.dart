174e7450040c272dcc453bd12bb1ab14c75edda2
Jawwad Farooq <jawwadfarooq22@yahoo.com>
2021-03-23 22:21:54 +0500

    Initial commit

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,24 @@
+import 'package:Shop_App/providers/orders.dart' show Orders;
+import 'package:Shop_App/widgets/app_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+        itemCount: orderData.orders.length,
+      ),
+    );
+  }
+}
